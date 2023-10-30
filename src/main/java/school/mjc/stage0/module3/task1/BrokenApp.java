a4a65ff25c11dccaa132fcfad058c572767f5f09
Amachuarori <52317097+Amachuarori@users.noreply.github.com>
2023-10-30 14:11:01 +0600

    my

--- src/main/java/school/mjc/stage0/module3/task1/BrokenApp.java
+++ src/main/java/school/mjc/stage0/module3/task1/BrokenApp.java
@@ -1,8 +1,10 @@
 package school.mjc.stage0.module3.task1;
 
+import static java.lang.System.*;
+
 public class BrokenApp {
     public static void main(String[] args)
     {
-        System.out.println("fix \nMe");
+        out.println("fixMe");
     }
 }
